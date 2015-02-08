0a59d1bf11e5b7696ea1f11465e4645b7f81f49d
AmasaDelano <alexleone89@gmail.com>
2015-02-08 01:07:10 -0600

    Versioning individual css and js files

---
+++ Aluminum/Controllers/HtmlHelperExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace Aluminum
+{
+    public static class HtmlHelperExtensions
+    {
+        private static readonly string _version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public static string Version(this HtmlHelper htmlHelper)
+        {
+            return _version;
+        }
+    }
+}
